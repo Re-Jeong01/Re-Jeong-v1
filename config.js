5b1774108aa5e91493df05f7ad14ded6d10fdf07
Takudzwa-Mlambo <157640942+Cod3Uchiha@users.noreply.github.com>
2024-08-01 19:42:53 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,25 @@
+//UBAH UBAH DISINI BRO JANGAN LUPA SUBREK YT DELTA TECH//
+//NOTE : cara menjadi premium silahkan Pergi ke file; all/database/premium.json
+//NOTE : cara menjadi publik owner silahkan Pergi ke file; all/database/owner.json
+
+require("./all/module")
+global.owner = "263789660160" 
+global.namaCreator = "" 
+global.autoJoin = false 
+global.antilink = false 
+global.versisc = '1.0' 
+global.codeInvite = ""
+global.isLink = ' ' 
+global.thumb = fs.readFileSync("./thumb.png") 
+global.audionya = fs.readFileSync("./all/sound.mp3") 
+global.packname = "BOT" 
+global.author = "YT : Cod3Uchiha" 
+global.jumlah = "5" 
+global.namabot = "MD-Killer" 
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update ${__filename}`))
+	delete require.cache[file]
+	require(file)
+})
